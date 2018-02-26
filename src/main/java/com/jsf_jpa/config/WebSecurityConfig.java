9d45b7b1956c1cb64fb8b0aa61ca253f8157dd42
Vitalij <berezin.v.v@gmail.com>
2018-02-26 11:28:45 +0200

    update security role

--- src/main/java/com/jsf_jpa/config/WebSecurityConfig.java
+++ src/main/java/com/jsf_jpa/config/WebSecurityConfig.java
@@ -2,17 +2,12 @@ package com.jsf_jpa.config;
 
 import com.jsf_jpa.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -28,8 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login","/registration","/logout").permitAll()
-                .antMatchers("/main","/page2").access("hasRole('USER')")
+                .antMatchers("/login", "/registration", "/javax.faces.resource/**", "/**","/logout").permitAll()
+                .antMatchers("/main", "/page2").access("hasRole('USER')")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
